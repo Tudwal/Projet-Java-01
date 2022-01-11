5ccb18cee77b8e4e2e81ec2f0af071119a1dbd1a
Tudwal CORLOUER <tudwal.corlouer2021@campus-eni.fr>
2022-01-11 16:22:19 +0100

    accueil JSP

---
+++ src/main/java/fr/eni/enchere/ihm/AccueilServlet.java
@@ -0,0 +1,49 @@
+package fr.eni.enchere.ihm;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import fr.eni.enchere.bll.EnchereManager;
+import fr.eni.enchere.bll.EnchereManagerSing;
+
+/**
+ * Servlet implementation class AccueilServlet
+ */
+@WebServlet("/AccueilServlet")
+public class AccueilServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	private EnchereManager manager = EnchereManagerSing.getInstance();
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AccueilServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		if (request.getParameter("connexion")!= null) {
+			request.getRequestDispatcher("WEB-INF/connexionCompte.jsp").forward(request, response);
+		}
+		
+		request.getRequestDispatcher("WEB-INF/accueil.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/accueil.jsp
@@ -0,0 +1,24 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Ebehhhhh</title>
+</head>
+<body>
+<h1>Ebehhhhhh</h1>
+<input type="submit" name="connexion" value="S'inscrire - Se connecter"/>
+<h2>Liste des enchères</h2>
+<p>Filtres</p> <br>
+<input type="search"/> <br>
+<select name="menu_categorie">
+	<option>Toutes </option>
+	<option>Informatique </option>
+	<option>Ameublement </option>
+	<option>Vetement </option>
+	<option>Sport&Loisirs </option>
+</select>
+
+</body>
+</html>
