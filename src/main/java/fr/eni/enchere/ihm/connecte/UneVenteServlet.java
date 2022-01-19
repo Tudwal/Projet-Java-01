4590aae146f1393df146e9005911a6e2c83f7b2e
Erwan PETREMOUL <erwan.petremoul2021@campus-eni.fr>
2022-01-19 12:24:15 +0100

    ihm

--- src/main/java/fr/eni/enchere/ihm/connecte/UneVenteServlet.java
+++ src/main/java/fr/eni/enchere/ihm/connecte/UneVenteServlet.java
@@ -1,11 +1,20 @@
 package fr.eni.enchere.ihm.connecte;
 
 import java.io.IOException;
+import java.util.Date;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import fr.eni.enchere.bll.BLLException;
+import fr.eni.enchere.bll.EnchereManager;
+import fr.eni.enchere.bll.EnchereManagerSing;
+import fr.eni.enchere.bo.ArticleVendu;
+import fr.eni.enchere.bo.Enchere;
+import fr.eni.enchere.bo.Utilisateur;
 
 /**
  * Servlet implementation class UneVenteServlet
@@ -13,6 +22,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/UneVenteServlet")
 public class UneVenteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	EnchereManager manager = EnchereManagerSing.getInstance();
 
 	/**
 	 * @see HttpServlet#HttpServlet()
@@ -27,7 +37,22 @@ public class UneVenteServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
+
+		if (request.getParameter("encherir") != null) {
+			Integer montantEnchere = Integer.parseInt(request.getParameter("montantEnchere"));
+			Utilisateur acheteur = ((UtilisateurModel) request.getSession().getAttribute("model")).getUtilisateur();
+			ArticleVendu article = ((UtilisateurModel) request.getSession().getAttribute("model")).getArticleVendu();
+			Date dateEnchere = new Date(System.currentTimeMillis());
+
+			Enchere enchere = new Enchere(article, acheteur, dateEnchere, montantEnchere);
+			try {
+				manager.encherire(enchere);
+			} catch (BLLException e) {
+				e.printStackTrace();
+			}
+
+		}
+
 		request.getRequestDispatcher("WEB-INF/afficherArticle.jsp").forward(request, response);
 	}
 
--- src/main/java/fr/eni/enchere/ihm/connecte/UtilisateurModel.java
+++ src/main/java/fr/eni/enchere/ihm/connecte/UtilisateurModel.java
@@ -3,27 +3,26 @@
  */
 package fr.eni.enchere.ihm.connecte;
 
+import fr.eni.enchere.bo.ArticleVendu;
 import fr.eni.enchere.bo.Utilisateur;
 
 public class UtilisateurModel {
-	
-	private Utilisateur utilisateur;
-	
-	private String message = "";
-	
-	
 
-	
-	
+	private Utilisateur utilisateur;
+
+	private String message = "";
+
+	private ArticleVendu articleVendu;
+
 	public UtilisateurModel() {
 		// TODO Auto-generated constructor stub
 	}
+
 	public UtilisateurModel(Utilisateur utilisateur) {
 		super();
 		this.utilisateur = utilisateur;
 	}
 
-	
 	public Utilisateur getUtilisateur() {
 		return utilisateur;
 	}
@@ -31,22 +30,34 @@ public class UtilisateurModel {
 	public void setUtilisateur(Utilisateur utilisateur) {
 		this.utilisateur = utilisateur;
 	}
-	
+
 	public String getMessage() {
 		return message;
 	}
+
 	public void setMessage(String message) {
 		this.message = message;
+	}
+
+	public ArticleVendu getArticleVendu() {
+		return articleVendu;
 	}
 
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
-		builder.append("EnchereModel [utilisateur=");
+		builder.append("UtilisateurModel [utilisateur=");
 		builder.append(utilisateur);
+		builder.append(", message=");
+		builder.append(message);
+		builder.append(", articleVendu=");
+		builder.append(articleVendu);
 		builder.append("]");
 		return builder.toString();
 	}
-	
-	
+
+	public void setArticleVendu(ArticleVendu articleVendu) {
+		this.articleVendu = articleVendu;
+	}
+
 }
